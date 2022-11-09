510860eee9ecfbc808cbc2f406a672531cf27493
Catalin_Buzgau <catalin_buzgau@epam.com>
2022-11-09 12:20:11 +0200

    Booking page restructure + form implementation with model attribute

--- src/main/java/com/springmvc/restaurant/controller/RestaurantController.java
+++ src/main/java/com/springmvc/restaurant/controller/RestaurantController.java
@@ -1,10 +1,22 @@
 package com.springmvc.restaurant.controller;
 
+import com.springmvc.restaurant.model.BookingAppointment;
+import com.springmvc.restaurant.repository.BookingRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class RestaurantController {
+    private BookingAppointment booking;
+    private BookingRepository bookingRepository;
+
+    public RestaurantController(BookingAppointment booking, BookingRepository bookingRepository) {
+        this.booking = booking;
+        this.bookingRepository = bookingRepository;
+    }
 
     @GetMapping("/home")
     public String getHomePage() {
@@ -27,7 +39,14 @@ public class RestaurantController {
     }
 
     @GetMapping("/booking")
-    public String getBookingPage() {
+    public String getBookingPage(Model model) {
+        model.addAttribute("bookings", new BookingAppointment());
+        return "booking";
+    }
+    @PostMapping("/booking")
+    public String postBookingForm(@ModelAttribute("bookings") BookingAppointment booking) {
+        bookingRepository.save(booking);
+        System.out.println(booking);
         return "booking";
     }
 
--- src/main/java/com/springmvc/restaurant/model/BookingAppointment.java
+++ src/main/java/com/springmvc/restaurant/model/BookingAppointment.java
@@ -1,10 +1,10 @@
 package com.springmvc.restaurant.model;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.util.Date;
 import java.util.Objects;
 
 @Entity
@@ -17,14 +17,15 @@ public class BookingAppointment {
     private String name;
     private String email;
     private String phoneNumber;
-    private LocalDate date;
-    private LocalTime time;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date date;
+    private String time;
     private int guestsNumber;
 
     public BookingAppointment() {
     }
 
-    public BookingAppointment(Integer id, String name, String email, String phoneNumber, LocalDate date, LocalTime time, int guestsNumber) {
+    public BookingAppointment(Integer id, String name, String email, String phoneNumber, Date date, String time, int guestsNumber) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -38,28 +39,56 @@ public class BookingAppointment {
         return id;
     }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
-    public LocalDate getDate() {
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Date getDate() {
         return date;
     }
 
-    public LocalTime getTime() {
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getTime() {
         return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public int getGuestsNumber() {
         return guestsNumber;
+    }
+
+    public void setGuestsNumber(int guestsNumber) {
+        this.guestsNumber = guestsNumber;
     }
 
     @Override
